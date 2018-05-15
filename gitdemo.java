1770ff00d7d222f99c1768d256cd6df906783ff4
Mamushe <34391825+Mamushe@users.noreply.github.com>
2018-05-15 09:43:19 +0300

    Update gitdemo.java
    from online

--- gitdemo.java
+++ gitdemo.java
@@ -2,5 +2,7 @@ Class One{
 	public static void main(String args[]){
 		system.out.println("hello");
 		system.out.println("hellooooooo");
+		system.out.println("hello");
+		system.out.println("hellooooooo");
 	}
 }
